2b31c8c7af90ae8a5525538709fa09b5a30496ed
Mahmoud El Sayed <ElSayedMA@students.rhu.edu.lb>
2024-03-14 12:49:41 +0200

    Update

--- M.A.H.A/Form1.cs
+++ M.A.H.A/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace M.A.H.A
 {
@@ -15,6 +16,23 @@ namespace M.A.H.A
         public Form1()
         {
             InitializeComponent();
+            RoleBox.Items.Add("Administrator");
+            RoleBox.Items.Add("Accountant");
+            RoleBox.Items.Add("Doctor");
+            RoleBox.Items.Add("Nurse");
+            RoleBox.Items.Add("Receptionist");
+
+            RoleBox.SelectedIndexChanged += RoleBox_SelectedIndexChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoleBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Selected item: {RoleBox.SelectedItem}");
         }
     }
 }
